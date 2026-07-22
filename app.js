5b87c4e8df6b33a7cc02c15e98cfc303f6917b66
Tan Kim Eng Samie <25032599@myrp.edu.sg>
2026-07-22 16:56:17 +0800

    Update server start message to include URL
    Samie changed the message 'Started server on port 3000' to 'Start server on http://localhost:3000'

--- app.js
+++ app.js
@@ -212,7 +212,7 @@ app.use('/jobs', browseRouter);
 // Run app on http://localhost:3000
 // =====================================================
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+    console.log('Server started on http://localhost:3000');
 });
 
 // Samie end of Login Page Assignment ===============================================================
